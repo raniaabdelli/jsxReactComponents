ef9fe4f3d1e341dc998e6def74a35a88b0d85502
rania abdelli <abdellirania1@gmail.com>
2023-01-23 15:03:17 +0100

    envoi checkpoint

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Address = () => {
+  return (
+    <h1>Rue el madina el mounawra Kalaa Sghuira Sousse</h1>
+  )
+}
+
+export default Address
---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FullName = () => {
+  return (
+    <h1 className="Full-Name">Rania Abdelli</h1>
+  )
+}
+
+export default FullName
